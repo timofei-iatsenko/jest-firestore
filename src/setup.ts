59d0100cf461a56206676ff6ee2badb48a2afd8f
Timofei Iatsenko <ty@road.travel>
2024-01-11 17:24:19 +0100

    chore: debug tests on ci

--- src/setup.ts
+++ src/setup.ts
@@ -4,9 +4,13 @@ import { startEmulator } from './emulator';
 const debug = require('debug')('jest-firestore:setup');
 
 module.exports = async (config: JestEnvironmentConfig['globalConfig']) => {
+  const options = getFirestoreEmulatorOptions(config.rootDir);
   const _shouldUseSharedDBForAllJestWorkers = shouldUseSharedDBForAllJestWorkers(config.rootDir);
 
   debug(`shouldUseSharedDBForAllJestWorkers: ${_shouldUseSharedDBForAllJestWorkers}`);
+
+  // this one is controversial, might override what user really wants, let's see
+  process.env.GCLOUD_PROJECT = options.project_id;
 
   // if we have FIRESTORE_EMULATOR_HOST already passed from env, bypass starting emulator
   if (process.env.FIRESTORE_EMULATOR_HOST) {
@@ -18,12 +22,8 @@ module.exports = async (config: JestEnvironmentConfig['globalConfig']) => {
 
   // if we run one emulator instance for all tests
   if (_shouldUseSharedDBForAllJestWorkers) {
-    const options = getFirestoreEmulatorOptions(config.rootDir);
     debug(`Starting Firestore Emulator`);
     process.env['FIRESTORE_EMULATOR_HOST'] = await startEmulator(options);
-
-    // this one is controversial, might override what user really wants, let's see
-    process.env.GCLOUD_PROJECT = options.project_id;
 
     global.__isEmulatorStarted = true;
   }
